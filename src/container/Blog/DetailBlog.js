70663b6299896fec56029bd9a2baaeda24521ae4
Namleez <Justinnlee696@gmail.com>
2025-04-04 16:36:02 +0700

    Fix comment

--- src/container/Blog/DetailBlog.js
+++ src/container/Blog/DetailBlog.js
@@ -137,6 +137,8 @@ function DetailBlog(props) {
                   })
                 }
 
+                // Comment Form
+
 
               </div>
               <CommentFormBlog handleAddComment={handleAddComment} />
